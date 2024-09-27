c8e2496a2b1bf8df1b385ced1565a333a644a93c
drarunas <arunas.radzvilavicius@nature.com>
2024-09-27 13:40:01 +0100

    Changes to be committed: 	new file:   app-dev-draft.py 	modified:   app.py 	modified:   requirements.txt

--- app-dev-draft.py
+++ app-dev-draft.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+
 from sentence_transformers import SentenceTransformer
 import torch
 import psycopg2
 from psycopg2 import pool
 import os
 from datetime import datetime
-import pandas as pd
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:8080"}})
-
-
 
 # Load the e5-multilingual-large model using SentenceTransformer
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -56,6 +54,7 @@ def index():
 
 # /query route to accept the 'q' parameter and return results
 @app.route('/query', methods=['GET'])
+
 # A function to query the database using the connection pool1
 def query():
     if request.method == 'GET':
@@ -76,75 +75,8 @@ def query():
 
                 # Step 3: Execute the SQL query using the new embedding
                 print(' 3 Executing sql query calculating distances', datetime.now().strftime("%H:%M:%S"))
-
-                sql_query='''
-                        SET LOCAL hnsw.ef_search = 20;
-                        WITH top_works AS (
-                            SELECT ev.id, 
-                                ev.embedding <=> %s::vector AS distance, 
-                                wt.title AS title
-                            FROM e5_vectors ev
-                            LEFT JOIN w_titles wt ON ev.id = wt.id
-                            ORDER BY distance
-                            LIMIT 20
-                        ),
-                        awd AS (
-                        SELECT tw.id, 
-                            tw.distance, 
-                            tw.title, 
-                            wa.auth_id auid
-                        FROM top_works tw
-                        LEFT JOIN w_auth wa ON tw.id = wa.id
-                        )
-                        SELECT awd.id, awd.title, awd.auid, awd.distance, a_names.orcid, a_names.name
-                        FROM awd 
-                        LEFT JOIN a_names ON awd.auid = a_names.id
-                        ORDER BY awd.distance
-                        '''
-                
-                cur.execute(sql_query, (new_embedding,))
-
-                results = cur.fetchall()
-                print(" 4 ok ", datetime.now().strftime("%H:%M:%S"))
-
-                results_list = [
-                    {"id": row[0], "title": row[1], "auid": row[2], "distance": row[3], "orcid": row[4], "name": row[5]} 
-                    for row in results
-                ]
-               
-                # Step 5: Release the connection back to the pool
-                pg_pool.putconn(conn)
-
-                return jsonify(results_list)
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    return jsonify({"error": str(e)}), 500
-
-@app.route('/queryauthors', methods=['GET'])
-# A function to query the database using the connection pool1
-def queryauthors():
-    if request.method == 'GET':
-        data = request.args.get('q')
-
-        if not data:
-            return jsonify({"error": "Missing query text"}), 400
-
-        print(" 1 getting embeddings from query ", datetime.now().strftime("%H:%M:%S"))
-        new_embedding = get_embeddings(data).tolist()
-
-        try:
-            # Step 2: Get a connection from the pool
-            print(" 2 Getting connection from the pool ", datetime.now().strftime("%H:%M:%S"))
-            conn = pg_pool.getconn()
-            if conn:
-                cur = conn.cursor()
-
-                # Step 3: Execute the SQL query using the new embedding
-                print(' 3 Executing sql query calculating distances', datetime.now().strftime("%H:%M:%S"))
                 
                 sql_query='''
-                    SET LOCAL hnsw.ef_search = 20;
 
                     WITH work_distances AS (
                         SELECT awd.auid, 
@@ -155,7 +87,7 @@ def queryauthors():
                             SELECT ev.id, ev.embedding <=> %s::vector AS distance
                             FROM e5_vectors ev
                             ORDER BY distance
-                            LIMIT 20
+                            LIMIT 50
                         ) AS top_works
                         JOIN w_auth wa ON top_works.id = wa.id
                         JOIN LATERAL (
@@ -164,7 +96,7 @@ def queryauthors():
                             JOIN w_auth wa2 ON ev.id = wa2.id
                             WHERE wa.auth_id = wa2.auth_id
                             ORDER BY ev.embedding <=> %s::vector
-                            LIMIT 3
+                            LIMIT 2
                         ) AS awd ON awd.auid = wa.auth_id
                         JOIN w_titles wt ON awd.work_id = wt.id
                         GROUP BY awd.auid, awd.work_id, wt.title
@@ -172,27 +104,41 @@ def queryauthors():
                     SELECT auid, work_id, distance,
                         AVG(distance) OVER (PARTITION BY auid) AS avg_distance_per_auid
                     FROM work_distances
-                    ORDER BY avg_distance_per_auid, distance
+                    ORDER BY avg_distance_per_auid, distance;
+
                     '''
 
                 cur.execute(sql_query, (new_embedding,new_embedding,new_embedding,))
-                # List of tuples
+
                 results = cur.fetchall()
+                print(results)
                 print(" 4 ok ", datetime.now().strftime("%H:%M:%S"))
-                # Return the connection to the pool
+
+                # Step 4: Convert results to a list of dictionaries for the template
+                # results_list = [
+                #     {"id": row[0], "title": row[1], "auid": row[2], "distance": row[3], "orcid": row[4], "name": row[5]} 
+                #     for row in results
+                # ]
+                results_list = {}
+                for row in results:
+                    author_id, work_id, distance, avg_distance = row
+                    
+                    if author_id not in results_list:
+                        results_list[author_id] = {
+                            'avg_distance': avg_distance,
+                            'works': []
+                        }
+                    
+                    results_list[author_id]['works'].append({
+                        'work_id': work_id,
+                        'distance': distance
+                    })
+                # Step 5: Release the connection back to the pool
                 pg_pool.putconn(conn)
 
-
-                # Convert list of tuples to DataFrame
-                df = pd.DataFrame(results, columns=['author', 'work', 'work_dist', 'avg_dist'])
-                grouped_data = (
-                    df.groupby(['author', 'avg_dist'])
-                    .apply(lambda x: x[['work', 'work_dist']].to_dict(orient='records'))
-                    .reset_index(name='works')
-                    .sort_values(by='avg_dist', ascending=True) 
-                ).to_dict(orient='records')
-                # Serialize your sorted_results
-                return jsonify(grouped_data)
+                sorted_results = sorted(results_list.items(), key=lambda item: item[1]['avg_distance'])
+                # return jsonify(results_list)
+                return jsonify(sorted_results)
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
